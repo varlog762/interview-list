5c17815c8923dabeb930f38e0e81f4e0d6e3d6d9
Gregory Pereverzev <boot762@gmail.com>
2024-10-04 18:53:31 +0300

    feat: add signIn method to user-store.ts

--- src/stores/user-store.ts
+++ src/stores/user-store.ts
@@ -3,7 +3,11 @@ import { ref, computed } from 'vue';
 import { defineStore } from 'pinia';
 import { FirebaseError } from 'firebase/app';
 
-import { firebaseSignUp } from 'src/services/firebase';
+import {
+  firebaseSignUp,
+  firebaseSignIn,
+  firebaseSignOut,
+} from 'src/services/firebase';
 import { getErrorMessage } from 'src/utils';
 import { AuthInputInterface } from 'src/models/auth-input.interface';
 import { AuthErrors } from 'src/enums';
@@ -15,6 +19,7 @@ export const useUserStore = defineStore('user', () => {
   const errorMessage = ref<string | null>(null);
   const isLoggedIn = computed<boolean>(() => !!userId.value);
 
+  // TODO: refactor signUp & signIn
   const signUp = async ({
     email,
     password,
@@ -23,6 +28,29 @@ export const useUserStore = defineStore('user', () => {
       isLoading.value = true;
       errorMessage.value = null;
       const userCredential = await firebaseSignUp(email, password);
+      isLoading.value = false;
+      return true;
+    } catch (error) {
+      isLoading.value = false;
+
+      if (error instanceof FirebaseError) {
+        errorMessage.value = getErrorMessage(error.code);
+        return false;
+      }
+
+      errorMessage.value = AuthErrors.UNKNOWN_ERROR;
+      return false;
+    }
+  };
+
+  const signIn = async ({
+    email,
+    password,
+  }: AuthInputInterface): Promise<boolean> => {
+    try {
+      isLoading.value = true;
+      errorMessage.value = null;
+      const userCredential = await firebaseSignIn(email, password);
       isLoading.value = false;
       return true;
     } catch (error) {
