1148e7dc159a1f1ca38fec1c01ae2f07982aaa34
Gregory Pereverzev <boot762@gmail.com>
2024-10-19 15:19:00 +0300

    feat: modify q-table with slots in InterviewListPage

--- src/pages/InterviewListPage.vue
+++ src/pages/InterviewListPage.vue
@@ -27,24 +27,18 @@ const columns: TableColumnsInterface[] = [
     align: 'left',
     field: 'companyName',
   },
+  { name: 'hrName', label: "HR's name", field: 'hrName', align: 'center' },
   {
     name: 'vacancyLink',
     label: 'Vacancy link',
     field: 'vacancyLink',
-    align: 'left',
-  },
-  { name: 'hrName', label: "HR's name", field: 'hrName' },
-  {
-    name: 'telegramUsername',
-    label: 'Telegram username',
-    field: 'telegramUsername',
+    align: 'center',
   },
   {
-    name: 'whatsAppUsername',
-    label: 'WhatsApp username',
-    field: 'whatsAppUsername',
+    name: 'contacts',
+    label: 'Contacts',
+    field: 'contacts',
   },
-  { name: 'hrPhoneNumber', label: 'Phone number', field: 'hrPhoneNumber' },
 ];
 
 onMounted(async () => {
@@ -65,7 +59,69 @@ onMounted(async () => {
       <div class="q-pa-md q-pt-xl">
         <h2 class="title-md">Your interviews</h2>
         <div class="q-pa-md">
-          <q-table :rows="interviews" :columns="columns" row-key="name" />
+          <q-table :rows="interviews" :columns="columns" row-key="name">
+            <!-- Custom slot for companyName -->
+            <template v-slot:body-cell-companyName="props">
+              <q-td :props="props">
+                <strong>{{ props.row.companyName }}</strong>
+              </q-td>
+            </template>
+
+            <template v-slot:body-cell-hrName="props">
+              <q-td :props="props">
+                <q-badge color="primary">
+                  {{ props.row.hrName }}
+                </q-badge>
+              </q-td>
+            </template>
+
+            <template v-slot:body-cell-vacancyLink="props">
+              <q-td :props="props">
+                <q-btn
+                  flat
+                  dense
+                  color="primary"
+                  :href="props.row.vacancyLink"
+                  target="_blank">
+                  Open Vacancy
+                </q-btn>
+              </q-td>
+            </template>
+
+            <template v-slot:body-cell-telegramUsername="props">
+              <q-td :props="props">
+                <a
+                  :href="`https://t.me/${props.row.telegramUsername}`"
+                  target="_blank">
+                  {{ props.row.telegramUsername }}
+                </a>
+              </q-td>
+            </template>
+            <template v-slot:body-cell-contacts="props">
+              <q-td :props="props">
+                <div class="flex justify-end q-gutter-xs">
+                  <a
+                    v-if="props.row.telegramUsername"
+                    :href="`https://t.me/${props.row.telegramUsername}`"
+                    target="_blank">
+                    <q-icon size="sm" name="telegram" color="primary" />
+                  </a>
+                  <a
+                    v-if="props.row.whatsAppUsername"
+                    :href="`https://wa.me/${props.row.whatsAppUsername}`"
+                    target="_blank">
+                    <q-icon size="sm" name="fab fa-whatsapp" color="positive" />
+                  </a>
+                  <a
+                    v-if="props.row.hrPhoneNumber"
+                    :href="`tel:${props.row.hrPhoneNumber}`"
+                    target="_blank">
+                    <q-icon size="sm" name="phone" color="info" />
+                  </a>
+                </div>
+              </q-td>
+            </template>
+          </q-table>
         </div>
       </div>
     </template>
