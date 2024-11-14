28f135ed86a7c515f72c264322ed59000a7e2af3
Gregory Pereverzev <boot762@gmail.com>
2024-11-14 15:41:53 +0300

    feat: modify StatisticsPage

--- src/pages/StatisticsPage.vue
+++ src/pages/StatisticsPage.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
-import { defineAsyncComponent } from 'vue';
+import { ref, defineAsyncComponent, onMounted } from 'vue';
 
+import SpinnerComponent from 'src/components/SpinnerComponent.vue';
 import NoInterviewComponent from 'src/components/NoInterviewComponent.vue';
 import { useInterviewStore } from 'src/stores';
 
@@ -12,19 +13,34 @@ const ChartComponent = defineAsyncComponent(
   () => import('src/components/ChartComponent.vue')
 );
 
+const isLoading = ref<boolean>(true);
+
 const interviewStore = useInterviewStore();
+
+onMounted(async () => {
+  try {
+    if (!interviewStore.interviewList.length) {
+      await interviewStore.fetchInterviews();
+    }
+  } finally {
+    isLoading.value = false;
+  }
+});
 </script>
 
 <template>
-  <template v-if="interviewStore.interviewList.length">
-    <div class="q-pa-md q-pt-xl">
-      <h2 class="title-md">Interview statistics</h2>
-      <ChartComponent :interview-list="interviewStore.interviewList" />
-    </div>
-  </template>
-
+  <SpinnerComponent v-if="isLoading" />
   <template v-else>
-    <NoInterviewComponent />
+    <template v-if="interviewStore.interviewList.length">
+      <div class="q-pa-md q-pt-xl">
+        <h2 class="title-md">Interview statistics</h2>
+        <ChartComponent :interview-list="interviewStore.interviewList" />
+      </div>
+    </template>
+
+    <template v-else>
+      <NoInterviewComponent />
+    </template>
   </template>
 </template>
 
