967fb9b8bb2218be874034508699c68a4053a76e
Gregory Pereverzev <boot762@gmail.com>
2025-01-16 16:15:30 +0300

    feat: add comments to interview and user stores

--- src/stores/interview-store.ts
+++ src/stores/interview-store.ts
@@ -9,13 +9,26 @@ import {
 import { useUserStore } from 'src/stores/';
 import useQuasarNotify from 'src/composables/useQuasarNotify';
 
+/**
+ * Interview store
+ */
 export const useInterviewStore = defineStore('interview', () => {
   const userStore = useUserStore();
   const showToast = useQuasarNotify();
 
+  /**
+   * The list of interviews for the current user
+   */
   const interviewList = ref<InterviewInputInterface[]>([]);
 
+  /**
+   * Fetches all interviews for the current user from Firestore.
+   * The interviews are retrieved in descending order of creation time.
+   * If the user is not logged in, the function does nothing.
+   */
   const fetchInterviews = async () => {
+    if (!userStore.isLoggedIn) return;
+
     try {
       interviewList.value = await firebaseGetAllInterviews(
         userStore.userId as string
@@ -25,7 +38,15 @@ export const useInterviewStore = defineStore('interview', () => {
     }
   };
 
+  /**
+   * Deletes an interview document with the given ID from Firestore.
+   * If the deletion is successful, it fetches the updated list of interviews.
+   * If the user is not logged in, the function does nothing.
+   * @param docId The ID of the interview document to delete.
+   */
   const deleteInterview = async (docId: string) => {
+    if (!userStore.isLoggedIn) return;
+
     try {
       await firebaseDeleteInterview(userStore.userId as string, docId);
       await fetchInterviews();
--- src/stores/user-store.ts
+++ src/stores/user-store.ts
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import { ref, computed } from 'vue';
 import { useRouter } from 'vue-router';
 import { defineStore } from 'pinia';
@@ -9,15 +8,36 @@ import type { AuthInputInterface } from 'src/models/auth-input.interface';
 import { RoutePathsEnum } from 'src/enums';
 import useQuasarNotify from 'src/composables/useQuasarNotify';
 
+/**
+ * User store
+ */
 export const useUserStore = defineStore('user', () => {
   const router = useRouter();
   const showToast = useQuasarNotify();
 
+  /**
+   * User ID
+   */
   const userId = ref<string | null>(null);
-  const isLoading = ref<boolean>(false);
+
+  /**
+   * Whether the auth state is ready
+   */
   const authReady = ref<boolean>(false);
+
+  /**
+   * Whether the user is logged in
+   */
   const isLoggedIn = computed<boolean>(() => !!userId.value);
 
+  /**
+   * Whether the store is currently handling an authentication request
+   */
+  const isLoading = ref<boolean>(false);
+
+  /**
+   * Initialize the auth state
+   */
   const initAuth = async (): Promise<void> => {
     const auth = getAuth();
     return new Promise<void>(resolve => {
@@ -29,14 +49,27 @@ export const useUserStore = defineStore('user', () => {
     });
   };
 
+  /**
+   * Sign up a user
+   * @param authInput The user's authentication input
+   */
   const signUp = (authInput: AuthInputInterface): void => {
     handleAuth(authInput, firebaseSignUp);
   };
 
+  /**
+   * Sign in a user
+   * @param authInput The user's authentication input
+   */
   const signIn = (authInput: AuthInputInterface): void => {
     handleAuth(authInput, firebaseSignIn);
   };
 
+  /**
+   * Handle an authentication request
+   * @param authInput The user's authentication input
+   * @param authCallback The authentication callback
+   */
   const handleAuth = async (
     authInput: AuthInputInterface,
     authCallback: (email: string, password: string) => Promise<UserCredential>
