b5fd31cafbddf9e0ec3c3578ff2b6021903d3a83
Gregory Pereverzev <boot762@gmail.com>
2025-01-20 16:44:29 +0300

    add comments to StatisticsPage

--- src/pages/StatisticsPage.vue
+++ src/pages/StatisticsPage.vue
@@ -1,22 +1,33 @@
 <script setup lang="ts">
+/**
+ * The StatisticsPage component is responsible for displaying the statistics of interviews.
+ * It fetches the interview data from the store asynchronously,
+ * shows a loading spinner while fetching the data, and conditionally displays either
+ * the interview statistics chart or a message that no interviews are available.
+ */
 import { ref, defineAsyncComponent, onMounted } from 'vue';
 
 import SpinnerComponent from 'src/components/SpinnerComponent.vue';
 import NoInterviewsComponent from 'src/components/NoInterviewsComponent.vue';
 import { useInterviewStore } from 'src/stores';
 
+// Component name for debugging and development purposes
 defineOptions({
   name: 'StatisticsPage',
 });
 
+// Dynamically importing the ChartComponent for lazy loading
 const ChartComponent = defineAsyncComponent(
   () => import('src/components/ChartComponent.vue')
 );
 
+// State to manage loading status
 const isLoading = ref<boolean>(true);
 
+// Initialize the interview store to manage the state of interviews
 const interviewStore = useInterviewStore();
 
+// Lifecycle hook that fetches interviews when the component is mounted
 onMounted(async () => {
   try {
     if (!interviewStore.interviewList.length) {
@@ -29,15 +40,19 @@ onMounted(async () => {
 </script>
 
 <template>
+  <!-- Show spinner while loading the data -->
   <SpinnerComponent v-if="isLoading" />
 
+  <!-- Show interview statistics if interviews are available -->
   <template v-else-if="interviewStore.interviewList.length">
     <div class="q-pa-md q-pt-xl">
       <h2 class="title-md">Interview statistics</h2>
+      <!-- Passing the interview list to the ChartComponent for visualization -->
       <ChartComponent :interview-list="interviewStore.interviewList" />
     </div>
   </template>
 
+  <!-- Display a fallback component if no interviews are available -->
   <NoInterviewsComponent v-else />
 </template>
 
